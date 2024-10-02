8808c417925130224af8115671433317bc48d9e7
Pierre Le Stunff <pls@pierres-mbp.home>
2024-10-02 23:16:56 +0200

    init

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,47 +1,60 @@
 <script lang="“ts”">
-	import Upload from './Upload.svelte';
+	import { onMount } from 'svelte';
+	import { invalidate } from '$app/navigation';
 
 	import { SpeedDial, SpeedDialButton, Button, Modal } from 'flowbite-svelte';
 	import { UploadSolid } from 'flowbite-svelte-icons';
 
-	let clickOutsideModal = false;
+	import Upload from './Upload.svelte';
 
+	let clickOutsideModal = false;
 	export let data;
+
+	onMount(() => {
+		const interval = setInterval(() => {
+			invalidate('data:now');
+		}, 1000);
+
+		return () => {
+			clearInterval(interval);
+		};
+	});
 </script>
 
-<svelte:head>
-	<title>Home</title>
-	<meta name="description" content="Svelte demo app" />
-</svelte:head>
-
-<table class="min-w-full divide-y divide-gray-200">
-	<thead class="bg-gray-50">
-		<tr>
-			<th
-				scope="col"
-				class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
-			>
-				ID
-			</th>
-			<!-- Add more headers as needed -->
-		</tr>
-	</thead>
-	<tbody class="bg-white divide-y divide-gray-200">
-		{#each data.files as file}
+<main>
+	<table class="min-w-full divide-y divide-gray-200">
+		<thead class="bg-gray-50">
 			<tr>
-				<td class="px-6 py-4 whitespace-nowrap">{file.key}</td>
+				<th
+					scope="col"
+					class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+				>
+					ID
+				</th>
+				<!-- Add more headers as needed -->
 			</tr>
-		{/each}
-	</tbody>
-</table>
+		</thead>
+		<tbody class="bg-white divide-y divide-gray-200">
+			{#each data.files as file}
+				<tr>
+					<td class="px-6 py-4 whitespace-nowrap">{file.key}</td>
+				</tr>
+			{/each}
+		</tbody>
+	</table>
 
+	<SpeedDial defaultClass="fixed end-6 bottom-6 z-50">
+		<SpeedDialButton name="Ajouter un nouveau document" on:click={() => (clickOutsideModal = true)}>
+			<UploadSolid class="w-6 h-6" />
+		</SpeedDialButton>
+	</SpeedDial>
 
-<SpeedDial defaultClass="fixed end-6 bottom-6 z-50">
-	<SpeedDialButton name="Ajouter un nouveau document" on:click={() => (clickOutsideModal = true)}>
-		<UploadSolid class="w-6 h-6" />
-	</SpeedDialButton>
-</SpeedDial>
+	<Modal title="Ajouter un nouveau document" bind:open={clickOutsideModal} autoclose>
+		<Upload />
+	</Modal>
+</main>
 
-<Modal title="Ajouter un nouveau document" bind:open={clickOutsideModal} autoclose>
-	<Upload />
-</Modal>
+<style>
+
+	
+</style>
--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -2,20 +2,40 @@
 	import { page } from '$app/stores';
 	import logo from '$lib/images/svelte-logo.svg';
 	import github from '$lib/images/github.svg';
-	import { DarkMode, Navbar, NavBrand, NavLi, NavUl, NavHamburger, Button, Input } from 'flowbite-svelte';
-	let btnClass = 'text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 rounded-lg text-xUfll p-2';
+	import {
+		DarkMode,
+		Navbar,
+		NavBrand,
+		NavLi,
+		NavUl,
+		NavHamburger,
+		Button,
+		Input
+	} from 'flowbite-svelte';
+	let btnClass =
+		'text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 rounded-lg text-xUfll p-2';
 </script>
 
+<header>
+	<Navbar rounded color="form">
+		<NavBrand href="/">
+			<span class="self-center whitespace-nowrap text-xl font-semibold dark:text-white"
+				>File Exchanger</span
+			>
+		</NavBrand>
+		<NavHamburger />
+		<NavUl>
+			<NavLi href="/" active={true}>Home</NavLi>
+			<NavLi href="/about">About</NavLi>
+		</NavUl>
+	</Navbar>
+</header>
 
-<Navbar rounded color="form">
-	<NavBrand href="/">
-	  <span class="self-center whitespace-nowrap text-xl font-semibold dark:text-white">File Exchanger</span>
-	</NavBrand>
-    <NavHamburger />
-	<NavUl>
-	  <NavLi href="/" active={true}>Home</NavLi>
-	  <NavLi href="/about">About</NavLi>
-	</NavUl>
-  </Navbar>	
 <style>
+	header {
+		position: fixed;
+		width: 100%;
+		top:0;
+		left: 0;
+	}
 </style>
