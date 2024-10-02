efa65929914f7ce3f0dad87823f73ca7ac979ebb
Pierre Le Stunff <pls@pierres-mbp.home>
2024-10-02 17:28:48 +0200

    init

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,17 +1,40 @@
 <script lang="“ts”">
 	import Upload from './Upload.svelte';
-	import Table from './Table.svelte';
 
 	import { SpeedDial, SpeedDialButton, Button, Modal } from 'flowbite-svelte';
 	import { UploadSolid } from 'flowbite-svelte-icons';
 
 	let clickOutsideModal = false;
+
+	export let data;
 </script>
 
 <svelte:head>
 	<title>Home</title>
 	<meta name="description" content="Svelte demo app" />
 </svelte:head>
+
+<table class="min-w-full divide-y divide-gray-200">
+	<thead class="bg-gray-50">
+		<tr>
+			<th
+				scope="col"
+				class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+			>
+				ID
+			</th>
+			<!-- Add more headers as needed -->
+		</tr>
+	</thead>
+	<tbody class="bg-white divide-y divide-gray-200">
+		{#each data.files as file}
+			<tr>
+				<td class="px-6 py-4 whitespace-nowrap">{file.key}</td>
+			</tr>
+		{/each}
+	</tbody>
+</table>
+
 
 <SpeedDial defaultClass="fixed end-6 bottom-6 z-50">
 	<SpeedDialButton name="Ajouter un nouveau document" on:click={() => (clickOutsideModal = true)}>
@@ -22,5 +45,3 @@
 <Modal title="Ajouter un nouveau document" bind:open={clickOutsideModal} autoclose>
 	<Upload />
 </Modal>
-
-<Table />
--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -1,3 +1,19 @@
 // since there's no dynamic data here, we can prerender
 // it so that it gets served as a static asset in production
 export const prerender = true;
+
+export const load = async ({ fetch }) => {
+    try {
+      const response = await fetch(
+        'https://app-8eb63342-ceae-4dd7-b32f-a6f573ef78de.cleverapps.io/files'
+      )
+      if (!response.ok) {
+        throw new Error(`HTTP error: ${response.status}`)
+      }
+      const files = await response.json()
+      return { files }
+    } catch (error) {
+      console.error(error)
+      return { error: 'Unable to fetch files' }
+    }
+  }
